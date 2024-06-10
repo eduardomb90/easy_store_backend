993e446a8f01acfc73914625ced5a14738fefdd8
edu_mb <eduardowemb@gmail.com>
2024-06-10 17:35:43 -0300

    Adição de data antes de salvar pedido

--- src/main/java/br/com/marques/easystore/controller/PedidoController.java
+++ src/main/java/br/com/marques/easystore/controller/PedidoController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marques.easystore.model.Cliente;
 import br.com.marques.easystore.model.Pedido;
+import br.com.marques.easystore.services.IClienteService;
 import br.com.marques.easystore.services.IPedidoService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.time.LocalDate;
 
 
 @RestController
@@ -16,8 +19,16 @@ public class PedidoController {
     @Autowired
     private IPedidoService service;
 
+     @Autowired
+    private IClienteService clienteService;
+
     @PostMapping("/pedido")
     public ResponseEntity<Pedido> inserirNovoPedido(@RequestBody Pedido novo) {
+        novo.setDataPedido(LocalDate.now());
+        
+        Cliente cliente = clienteService.atualizarDados(novo.getCliente());
+        
+        novo.setCliente(cliente); // atualiza para o cliente que agora já está salvo no Banco de dados.
         novo = service.inserirPedido(novo);
 
         if (novo != null) {
